a407d6dc33b746bc05e15f3df0956a61053dddca
Carolyn <connorscarolyns@gmail.com>
2021-06-09 15:47:11 -0300

    add tabs

--- back.php
+++ back.php
@@ -4,16 +4,17 @@
     <meta charset="utf-8"><!--Meta data is page specific and should be assess and adjusted on a per page basis.-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortlink" href="https://beta.novascotia.ca/">
-    
+
     <title>Compnent Overview - Service Pattern Library - Government of Nova Scotia</title>
-        
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><!-- Should be included in all head tags -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script><!-- Should be included in all head tags -->    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script><!-- Should be included in all head tags -->    
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script><!-- Not needed for service design, loads the navigation for this site -->  
-    <link rel="stylesheet" media="all" href="https://novascotia.ca/clf/fonts/font-awesome/css/font-awesome.min.css">        
-    <link rel="stylesheet" media="all" href="css/saltire.css">
-    <link rel="stylesheet" media="all" href="css/supplimentary-page-styles.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script><!-- Should be included in all head tags -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script><!-- Should be included in all head tags -->
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script><!-- Not needed for service design, loads the navigation for this site -->
+    <link rel="stylesheet" media="all" href="https://novascotia.ca/clf/fonts/font-awesome/css/font-awesome.min.css">
+  	<link rel="stylesheet" media="all" href="css/saltire.css"><!-- Include in all head tags -->
+  	<link rel="stylesheet" media="all" href="css/service-specific-styles.css"><!-- Not needed for services-->
+  	<link rel="stylesheet" media="all" href="css/supplimentary-page-styles.css"><!-- Include in all head tags -->
     <link rel="stylesheet" media="all" href="css/global-form-styles.css">
     <link rel="stylesheet" media="all" href="css/phase-banner.css"><!-- Include this only if needed -->
     <!--[if lte IE 8]>
@@ -93,61 +94,83 @@ $(function(){
                             <div class="col-md-12 col-xs-12">
                                 <h2>Back link example</h2>
                                 <div class="exampleDisplay">
-                                    <?php
-                                        $files = array();
-                                        $handle=opendir('back');
-                                            while (false !== ($file = readdir($handle))):
-                            if(substr($file, -5) == '.html'):
-                                $files[] = $file;
-                            endif;
-                        endwhile;
-                        sort($files);
-                            echo '<div class="pattern">';
-                            echo '<div class="display">';
-                            include('back/back.html');
-                            echo '</div>';
-                            echo '<div class="source">';
-                            echo '<textarea rows="6" cols="15">';
-                            echo htmlspecialchars(file_get_contents('back/back.html'));
-                            echo '</textarea>';
-                            echo '<p><a href="back/back.html"></a></p>';
-                            echo '</div>';
-                            echo '</div>';
-                        ?>
+                                      <?php
+                                                      $files = array();
+                                                      $handle=opendir('back');
+                                                          while (false !== ($file = readdir($handle))):
+                                          if(substr($file, -5) == '.html'):
+                                              $files[] = $file;
+                                          endif;
+                                      endwhile;
+                                      sort($files);
+                                          echo '<div class="pattern">';
+                                          echo '<div class="display">';
+                                          include('back/back.html');
+                                          echo '</div>';
+                                          echo '<div class="source">';
+                                          echo '<textarea rows="6" cols="15">';
+                                          echo htmlspecialchars(file_get_contents('back/back.html'));
+                                          echo '</textarea>';
+                                          echo '<p><a href="back/back.html"></a></p>';
+                                          echo '</div>';
+                                          echo '</div>';
+                                      ?>
                                  </div>
                             </div><!-- / example col -->
                         </section><!--/row-->
-                        
+
                         <h2 id="how-it-works">Back to start</h2>
                             <p>Include a back to start button on the confirmation page. Research has shown that some users like to have a closing action after they have completed thier transaction. It signals to them there that there is nothing left for them to do and it's safe for them to close the page. It should be placed at the bottom of the <a href="panel.html"> confirmation panel</a>.</p>
-                            
+
                             <section class="row">
                                 <div class="col-md-12 col-xs-12">
-                                    <h2>Back to start example</h2>
-                                    <div class="exampleDisplay">
-                                     <?php
-                                        $files = array();
-                                        $handle=opendir('back');
-                                            while (false !== ($file = readdir($handle))):
-                            if(substr($file, -5) == '.html'):
-                                $files[] = $file;
-                            endif;
-                        endwhile;
-                        sort($files);
-                            echo '<div class="pattern">';
-                            echo '<div class="display">';
-                            include('back/back-to-start.html');
-                            echo '</div>';
-                            echo '<div class="source">';
-                            echo '<textarea rows="6" cols="15">';
-                            echo htmlspecialchars(file_get_contents('back/back-to-start.html'));
-                            echo '</textarea>';
-                            echo '<p><a href="back/back-to-start.html"></a></p>';
-                            echo '</div>';
-                            echo '</div>';
-                        ?>   
+                                  <h2>Back to start example</h2>
+                                  <div class="exampleDisplay">
+                                    <!-- Tab nav -->
+                                    <ul class="nav nav-tabs" role="tablist">
+                                      <li class="nav-item">
+                                        <a class="nav-link active" href="#exampleBackToStart" role="tab" data-toggle="tab">Example</a>
+                                      </li>
+                                      <li class="nav-item">
+                                        <a class="nav-link" href="#htmlBackToStart" role="tab" data-toggle="tab">HTML</a>
+                                      </li>
+                                    </ul>
+                                    <!-- Tab panes -->
+                                    <div class="tab-content">
+                                      <div role="tabpanel" class="tab-pane fade in active show" id="exampleBackToStart">
+                                          <?php
+                                             $files = array();
+                                             $handle=opendir('back');
+                                                 while (false !== ($file = readdir($handle))):
+                                                 if(substr($file, -5) == '.html'):
+                                                     $files[] = $file;
+                                                 endif;
+                                             endwhile;
+                                             sort($files);
+                                                 echo '<div class="pattern">';
+                                                 echo '<div class="display">';
+                                                 include('back/back-to-start.html');
+                                                 echo '</div>';
+                                                 echo '</div>';
+                                             ?>
+                                      </div>
+                                      <div role="tabpanel" class="tab-pane fade" id="htmlBackToStart">
+                                        <?php
+                                           $files = array();
+                                           $handle=opendir('back');
+                                               while (false !== ($file = readdir($handle))):
+                                               if(substr($file, -5) == '.html'):
+                                                   $files[] = $file;
+                                               endif;
+                                           endwhile;
+                                           sort($files);
+                                               echo '<textarea rows="6" cols="15">';
+                                               echo htmlspecialchars(file_get_contents('back/back-to-start.html'));
+                                           ?>
+                                      </div>
                                     </div>
-                                </div><!-- / example col -->
+                                  </div>
+                              </div><!-- / example col -->
                             </section><!--/row-->
                         </div>
                     </div>
@@ -160,6 +183,25 @@ $(function(){
 </div>
 
 </body>
-</html>        
-             
-                
+</html>
+
+<?php
+   $files = array();
+   $handle=opendir('back');
+       while (false !== ($file = readdir($handle))):
+       if(substr($file, -5) == '.html'):
+           $files[] = $file;
+       endif;
+   endwhile;
+   sort($files);
+       echo '<div class="pattern">';
+       echo '<div class="display">';
+       include('back/back-to-start.html');
+       echo '</div>';
+       echo '<div class="source">';
+       echo '<textarea rows="6" cols="15">';
+       echo htmlspecialchars(file_get_contents('back/back-to-start.html'));
+       echo '</div>';
+       echo '</div>';
+   ?>
+ </div>
